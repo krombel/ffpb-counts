83f4468b788fb1f8894f84beaf5ad73a41e9a6b4
Krombel <mail@krombel.de>
2015-01-21 09:15:16 +0100

    updated calculating script to ne version of nodes.json; removed header of index-site

--- old-calcCounts.py
+++ old-calcCounts.py
@@ -8,8 +8,11 @@ nodes_count = 0
 clients_count = 0
 
 for node in data['nodes']:
-	if node['flags']['online'] and not node['flags']['gateway']:
+	if node['flags']['online'] and not node['flags']['gateway'] and not node['flags']['client']:
 		nodes_count += 1
-		clients_count += node['clientcount']
+	
+for link in data['links']:
+	if link['type'] == 'client':
+		clients_count += 1
 
 print('nodes.count: {:d} - clients.count: {:d}'.format(nodes_count, clients_count))
